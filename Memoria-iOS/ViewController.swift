e28241d29e4ff29e06d1baba109c5846d8c6e3c8
村松龍之介 <riscait@gmail.com>
2018-10-25 22:44:10 +0900

    Initial Commit

---
+++ Memoria-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memoria-iOS
+//
+//  Created by 村松龍之介 on 2018/10/25.
+//  Copyright © 2018 nerco studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
