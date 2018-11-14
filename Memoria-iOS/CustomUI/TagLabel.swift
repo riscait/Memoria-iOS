8b9eaa290ca1e7f108e897de19035fd7059ec74f
村松 龍之介 <mr@nerco.jp>
2018-11-14 21:41:19 +0900

    Add Pods/

---
+++ Memoria-iOS/CustomUI/TagLabel.swift
@@ -0,0 +1,21 @@
+//
+//  TagLabel.swift
+//  Memoria-iOS
+//
+//  Created by 村松龍之介 on 2018/11/10.
+//  Copyright © 2018 nerco studio. All rights reserved.
+//
+
+import UIKit
+
+class TagLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
