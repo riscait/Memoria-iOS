4e0e5fb85ef8fba759e10492594e50fb243d5726
Riscait <mr@nerco.jp>
2019-04-07 10:57:44 +0900

    タイムライン画面用のファイルを追加しました。（中身は未実装）

---
+++ Memoria-iOS/Classes/Views/TimelineCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimelineCell.swift
+//  Memoria-iOS
+//
+//  Created by 村松龍之介 on 2019/04/02.
+//  Copyright © 2019 nerco studio. All rights reserved.
+//
+
+import UIKit
+
+final class TimelineCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
---
+++ Memoria-iOS/Classes/Views/TimelineVC.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineVC.swift
+//  Memoria-iOS
+//
+//  Created by 村松龍之介 on 2019/04/02.
+//  Copyright © 2019 nerco studio. All rights reserved.
+//
+
+import UIKit
+
+final class TimelineVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
